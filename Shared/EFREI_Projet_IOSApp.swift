7e1688225f9c53990c2fdb8cd7f750572c34e213
Celipot <cyrus.elipot@gmail.com>
2021-03-16 14:47:27 +0000

    empty project

---
+++ Shared/EFREI_Projet_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFREI_Projet_IOSApp.swift
+//  Shared
+//
+//  Created by user188220 on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct EFREI_Projet_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
