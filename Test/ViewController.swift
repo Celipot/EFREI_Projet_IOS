993ec10398a8bad60bf8509d36e8f1b6e96d7fc0
Chengqian <chengqian.b@gmail.com>
2021-03-31 16:23:02 +0100

    renamed main to networkmoved controller code to the correct block

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -12,6 +12,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Controller
+        let requestFactory = RequestFactory()
+
+        requestFactory.getFurnitureList { (errorHandle, furnitures) in
+            if
+                let _ = errorHandle.errorType, let errorMessage =
+                    errorHandle.errorMessage {
+                print(errorMessage)
+                
+            }else if let list = furnitures, let furniture = list.last {
+                print(furniture.id)
+                
+            }else {print("Houston we got a problem")    }
+            
+        }
+
+        requestFactory.deleteFurniture(with: "keyZoaahVuD0KvuoM") { (errorHandle, response) in
+            if let _ = errorHandle.errorType, let errorMessage = errorHandle.errorMessage {
+                print(errorMessage)
+                
+            }
+            else if let reponse = response {
+                print(reponse.deleted)
+
+                
+            }
+            else {print("Houston we got a problem")
+                
+            }
+            
+        }
     }
 
 
--- Test/network.swift
+++ Test/network.swift
@@ -133,34 +133,3 @@ class RequestFactory: RequestFactoryProtocol {
         
     }
 }
-// Controller
-let requestFactory = RequestFactory()
-
-requestFactory.getFurnitureList { (errorHandle, furnitures) in
-    if
-        let _ = errorHandle.errorType, let errorMessage =
-            errorHandle.errorMessage {
-        print(errorMessage)
-        
-    }else if let list = furnitures, let furniture = list.last {
-        print(furniture.id)
-        
-    }else {print("Houston we got a problem")    }
-    
-}
-
-requestFactory.deleteFurniture(with: "keyZoaahVuD0KvuoM") { (errorHandle, response) in
-    if let _ = errorHandle.errorType, let errorMessage = errorHandle.errorMessage {
-        print(errorMessage)
-        
-    }
-    else if let reponse = response {
-        print(reponse.deleted)
-
-        
-    }
-    else {print("Houston we got a problem")
-        
-    }
-    
-}
